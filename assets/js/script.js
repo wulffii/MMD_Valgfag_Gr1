35a77487fa840b539e460c38f6d6998292e2cf3c
Lynkineseren <jantran147@gmail.com>
2025-09-19 11:43:12 +0200

    kommentar og reference til kode på landing page og food story

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Hjerte ikonet farve vil ændre sig så snart der bliver klikket på den. Hjertet styles inde på css'en.
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-heart")) {
     e.target.classList.toggle("liked");
